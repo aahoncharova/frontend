5c298103c8ce002559d26aed24c46f370fe61a9f
AnutkaGn <annaartemivna@gmail.com>
2026-03-28 12:44:23 +0200

    feat: implemented task 3 in lab 8 (live validation + submitting state)

--- lab-08-forms-validation/src/components/AuthForm.vue
+++ lab-08-forms-validation/src/components/AuthForm.vue
@@ -1,33 +1,34 @@
 <template>
   <form @submit.prevent="submitForm" class="auth-form" novalidate>
-    <input v-model="form.name" type="text" placeholder="Name" />
-    <div v-if="errors.name" class="error-msg">{{ errors.name }}</div>
+    <input v-model="form.name" type="text" placeholder="Name" @blur="onBlur('name')" />
+    <div v-if="touched.name && errors.name" class="error-msg">{{ errors.name }}</div>
 
-    <input v-model="form.email" type="email" placeholder="Email" />
-    <div v-if="errors.email" class="error-msg">{{ errors.email }}</div>
+    <input v-model="form.email" type="email" placeholder="Email" @blur="onBlur('email')" />
+    <div v-if="touched.email && errors.email" class="error-msg">{{ errors.email }}</div>
 
-    <input v-model="form.password" type="password" placeholder="Password" />
-    <div v-if="errors.password" class="error-msg">{{ errors.password }}</div>
+    <input v-model="form.password" type="password" placeholder="Password" @blur="onBlur('password')" />
+    <div v-if="touched.password && errors.password" class="error-msg">{{ errors.password }}</div>
 
-    <input v-model="form.confirmPassword" type="password" placeholder="Confirm Password" />
-    <div v-if="errors.confirmPassword" class="error-msg">{{ errors.confirmPassword }}</div>
+    <input v-model="form.confirmPassword" type="password" placeholder="Confirm Password" @blur="onBlur('confirmPassword')" />
+    <div v-if="touched.confirmPassword && errors.confirmPassword" class="error-msg">{{ errors.confirmPassword }}</div>
 
-    <input v-model.number="form.age" type="number" min="0" placeholder="Age" />
-    <div v-if="errors.age" class="error-msg">{{ errors.age }}</div>
+    <input v-model.number="form.age" type="number" min="0" placeholder="Age" @blur="onBlur('age')" />
+    <div v-if="touched.age && errors.age" class="error-msg">{{ errors.age }}</div>
 
     <label class="checkbox-label">
-      <input v-model="form.agree" type="checkbox" />
+      <input v-model="form.agree" type="checkbox" @blur="onBlur('agree')" />
       I agree to the terms
     </label>
-    <div v-if="errors.agree" class="error-msg">{{ errors.agree }}</div>
+    <div v-if="touched.agree && errors.agree" class="error-msg">{{ errors.agree }}</div>
 
-    <button type="submit">Send</button>
+    <div v-if="successMsg" class="success-msg">{{ successMsg }}</div>
+    <button type="submit" :disabled="isSubmitting">{{ isSubmitting ? 'Sending...' : 'Send' }}</button>
   </form>
 </template>
 
 <script setup>
 import { useValidator } from '../composables/useValidator'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 const form = reactive({
   name: '',
@@ -37,13 +38,72 @@ const form = reactive({
   age: '',
   agree: false
 })
+const touched = reactive({
+  name: false,
+  email: false,
+  password: false,
+  confirmPassword: false,
+  age: false,
+  agree: false
+})
 
-const { errors, validateAll } = useValidator()
+function onBlur(field) {
+  touched[field] = true
+  switch (field) {
+    case 'name':
+      validateName(form.name)
+      break
+    case 'email':
+      validateEmail(form.email)
+      break
+    case 'password':
+      validatePassword(form.password)
+      validateConfirmPassword(form.password, form.confirmPassword)
+      break
+    case 'confirmPassword':
+      validateConfirmPassword(form.password, form.confirmPassword)
+      break
+    case 'age':
+      validateAge(form.age)
+      break
+    case 'agree':
+      validateAgree(form.agree)
+      break
+  }
+}
+
+const { errors, validateAll, validateName, validateEmail, validatePassword, validateConfirmPassword, validateAge, validateAgree } = useValidator()
+
+const isSubmitting = ref(false)
+const successMsg = ref('')
+
+function resetForm() {
+  form.name = ''
+  form.email = ''
+  form.password = ''
+  form.confirmPassword = ''
+  form.age = ''
+  form.agree = false
+  Object.keys(touched).forEach(k => touched[k] = false)
+}
 
 function submitForm() {
-  if (!validateAll(form)) return
-  console.log('Form submitted:', { ...form })
+  successMsg.value = ''
+  if (!validateAll(form)) {
+    // Mark all as touched to show errors if user tries to submit without touching
+    Object.keys(touched).forEach(k => touched[k] = true)
+    return
+  }
+  isSubmitting.value = true
+  setTimeout(() => {
+    isSubmitting.value = false
+    successMsg.value = 'Form submitted successfully!'
+    resetForm()
+    Object.keys(errors.value).forEach(k => errors.value[k] = '')
+  }, 1500)
 }
+
+// No live validation, only onBlur
 </script>
 
 <style scoped>
@@ -89,5 +149,12 @@ function submitForm() {
   font-size: 0.98rem;
   margin: -10px 0 8px 2px;
   padding-left: 2px;
+}
+
+.success-msg {
+  color: #27ae60;
+  font-size: 1.05rem;
+  margin-bottom: 10px;
+  text-align: center;
 }
 </style>
